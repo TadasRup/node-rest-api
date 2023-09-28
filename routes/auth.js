a611948c3cda6c984123a3497d8adb6fc11bcf68
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-09-28 09:03:04 +0300

    Hidden Password

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,27 @@
 import express from 'express';
 const router = express.Router();
 import User from "../models/User.js";
+import bcrypt from "bcrypt"
 
 //REGISTER
 router.post("/register", async (req, res) => {
-   const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-   });
-
    try{
+//GENERATE NEW PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//CREATE NEW USER
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+//SAVE USER AND RETURN SESPONSE
     const user = await newUser.save();
     res.status(200).json(user);
    } catch(err){
         console.log(err)
    }
 })
-
 
 
 router.get("/",(req, res)=>{
